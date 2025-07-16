69cdae6d6ec5229865f112d23d8411d9029462e4
Saish Mungase <saishmungase@gmail.com>
2025-07-16 23:54:53 +0530

    add : generated a worker to manage jobs using queue and hashmap

---
+++ backend/api/jobManager.js
@@ -0,0 +1,48 @@
+import {v4 as uuid} from 'uuid'
+import Job from './job';
+
+class JobManager{
+
+    constructor(){
+        this.queue = [];
+        this.map = new Map();
+        this.running = false;
+        setInterval(() => this.executeJob(), 2000);
+    }
+
+    addJob(jobName, jobCode){
+        const jobId = uuid();
+
+        const job = new Job(jobId, jobName, jobCode);
+
+        this.queue.push(jobId);
+        this.map.set(jobId, job);
+
+        return jobId;
+    }
+
+    executeJob(){
+
+        if(this.queue.length == 0 || this.running) return;
+
+        const jobId = this.queue[0];
+        const job = this.map.get(jobId);
+
+        if (!job || job.status !== 'waiting') {
+          this.queue.shift(); 
+          return;
+        }
+        this.running = true;
+        job.executeJob();
+
+        const checkInterval = setInterval(() => {
+          if (job.status === 'completed' || job.status === 'failed') {
+            clearInterval(checkInterval);
+            this.queue.shift(); 
+            this.running = false;
+          }
+        }, 1000);
+    }
+}
+
+export default JobManager
